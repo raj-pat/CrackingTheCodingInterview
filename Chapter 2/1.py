2fc66f33236180297793ffd8c1ebfba0f279cd6c
rajp4 <rajp4445@gmail.com>
2019-09-22 00:44:47 -0400

    Start chapter 2. Solve question 1

---
+++ Chapter 2/1.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+
+def defLL(LL):
+    last = Node(LL[0])
+    first = last
+    for val in LL[1:]:
+        newNode = Node(val, None)
+        last.next = newNode
+        last = newNode
+    return first
+
+
+# To remove the duplicates from the linked list
+import importlib
+
+importlib.import_module("localLinkedList")
+
+
+def removeDup(startNode):
+    currNode = startNode
+    last = startNode
+    buffer = []
+    buffer.append(currNode.val)
+    while (currNode != None):
+        currDeleted = False
+        if currNode.val in buffer:  # check if the next value exist in the set. If it does, remove the next from the LL
+            last.next = currNode.next
+            currDeleted = True
+        else:
+            buffer.append(currNode.val)
+        if currDeleted is False:
+            last = currNode
+        currNode = currNode.next
+
+    while (startNode != None):
+        print(str(startNode.val) + "-> ", end="")
+        startNode = startNode.next
+
+
+removeDup(defLL([3, 3, 3, 3, 3, 3, 3, 3]))
